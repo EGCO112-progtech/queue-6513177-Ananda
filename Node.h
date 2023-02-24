9210fea6f32abb6b47b615fb32a7ddf97cc8f1ed
6513177-Ananda <ananda.kon@student.mahidol.edu>
2023-02-24 17:31:54 +0700

    Finish version

--- Node.h
+++ Node.h
@@ -7,37 +7,48 @@
 #ifndef Node_h
 #define Node_h
 
-struct node
-{
-    int data;
-    struct node *nextPtr;
-};
+
+typedef struct node{
+   struct node* next;
+   int order_number;
+   int quan;
+}order;
 
 typedef struct node Node;
 typedef struct node* NodePtr;
 
 
-void enqueue(NodePtr * head, NodePtr* tail, int x){
-  Node* new_node=(NodePtr) malloc(sizeof(Node));
-if(new_node){ 
-    /* Finish queue*/
- }
-}
+// void enqueue(NodePtr * head, NodePtr* tail, int x){
+//   Node* new_node=(NodePtr) malloc(sizeof(Node));
+//   new_node->data = x;
+//   new_node->next = NULL;
+//   if(new_node){ 
+
+//     if (*head == NULL)
+//     {
+//       *head = new_node;
+//       *tail = new_node;
+//     }
+
+//     (*tail)->next = new_node;
+//     (*tail) = new_node;
+//     return;
+//   }
+// }
 
 
-int dequeue(NodePtr* head, NodePtr* tail){
-  NodePtr t=*head;
-   if(t){
-   int value= t->data;
-   /* Finish dequeue*/
-       
-       
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
-}
-
-
+// int dequeue(NodePtr* head, NodePtr* tail){
+//   NodePtr t=*head;
+//    if(t){
+//     int value= t->data;
+//     /* Finish dequeue*/
+//     *head = (*head)->next;
+//     if (!(*head)) *tail = NULL;
+//     free(t);
+//     return value;
+//    }
+//    printf("Empty queue");
+//    return 0;
+// }
 
 #endif
--- Queue.h
+++ Queue.h
@@ -1,27 +1,73 @@
 
+// #include "Node.h"
 
-typedef struct {
-	 NodePtr headPtr,tailPtr;
-	int size;
-}Queue;
+typedef struct{
+  NodePtr head, tail;
+  int size;
+} Queue;
 
-
-void enqueue_struct(Queue* q, int x){
+void enqueue_struct(Queue* q, int order_num, int quant){
   Node *new_node=(Node*) malloc(sizeof(Node));
-if(new_node){ 
-  /*Finish enqueue */
- }
+  printf("My order is: %d\n", order_num);
+  if(new_node)
+  { 
+    new_node->order_number = order_num;
+    new_node->quan = quant;
+    new_node->next = NULL;
+    if (q->head == NULL)
+    {
+      q->head = new_node;
+      q->tail = new_node;
+    } else q->tail->next = new_node;
+    q->tail = new_node;
+    q->size++;
+  }
 }
 
 
-int dequeue_struct(Queue *q){
-   NodePtr t=q->headPtr;
-   if(t){
-   int value= t->data;
-       /*Finish dequeue */
-   return value;
+void dequeue_struct(Queue *q){
+   NodePtr t=q->head;
+   int cost = 0;
+   int money = 1;
+   if(t)
+   {
+    int order_num = t->order_number;
+    int quan = t->quan;
+    switch (order_num)
+      {
+        case 1: 
+          printf("Ramen\n");
+          cost = 100 * quan;
+          printf("You have to pay %d\n", cost);
+          break;
+        case 2:
+          printf("Somtum\n"); 
+          cost = 20 * quan;
+          printf("You have to pay %d\n", cost);
+          break;
+        case 3: 
+          printf("Fried Chicken\n"); 
+          cost = 50 * quan;
+          printf("You have to pay %d\n", cost);
+          break;
+        default:
+          printf("No Food\n");
+          cost = 0;
+          break;
+      }
+    while (money < cost)
+    {
+      printf("Cash: ");
+      scanf("%d", &money);
+    }
+
+    q->head = q->head->next;
+    if (q->head == NULL) q->tail = NULL;
+    free(t);
+    q->size--;
+    return;
    }
-   printf("Empty queue");
-   return 0;
+  //  printf("Empty queue");
+   return;
 }
 
